71cbce497bd291b06ca460a60e95a9fe32c79dbc
ThomasJazz <ttjazztt@gmail.com>
2018-10-20 09:29:12 -0700

    first

---
+++ app/src/main/java/com/thomasdevelops/bathroomappmaps/GoogleMapsFragment.java
@@ -0,0 +1,57 @@
+package com.thomasdevelops.bathroomappmaps;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GoogleMapsFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    public GoogleMapsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng ew = new LatLng(46.9965144, -120.5478474);
+        MarkerOptions option = new MarkerOptions();
+        option.position(ew).title("Ellensburg, Washington");
+        map.addMarker(option);
+        map.moveCamera(CameraUpdateFactory.newLatLng(ew));
+    }
+}
