9b98063e509d78fcd251ff564af2a31268aa9a4f
ThomasJazz <ttjazztt@gmail.com>
2018-10-20 14:44:24 -0700

    - Made add fire dialog but have not yet linked it with anything

--- app/src/main/java/com/thomasdevelops/spaceappsfuego/MapsActivity.java
+++ app/src/main/java/com/thomasdevelops/spaceappsfuego/MapsActivity.java
@@ -2,6 +2,7 @@ package com.thomasdevelops.spaceappsfuego;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -42,4 +43,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
 }
